ab1abb5d10f84aa51b54159b774f8694edcb968d
Veronica Gonzalez <veronicagonzalez@serviciodigital.cl>
2022-09-20 09:10:51 -0300

    Informacion sobre Math, Incrementos, Template String y Booleanos

--- js/apuntes.js
+++ js/apuntes.js
@@ -62,3 +62,71 @@ console.log('La resta de ambos números es: ', numero1 - numero2);
 console.log('La multiplicación de ambos números es: ', numero1 * numero2);
 console.log('La división de ambos números es: ', numero1 / numero2);
 console.log('El módulo es: ', numero1 % numero2);
+
+// Objeto Math
+// En la consola se puede poner la palabra "window" y saldrán las funciones de JavaScript
+// Se puede poner el comando "window.Math.random()" y esto lanzará un número aleatorio, también se puede poner solo "Math.random()"
+
+const pi = Math.PI; //Pi
+const round = Math.round(2.5); //Redondea 
+const ceil = Math.ceil(2.1); //Redondea siempre hacia arriba
+const floor = Math.floor(2.9); //Redondea siempre hacia abajo
+const sqrt = Math.sqrt(144); //Raiz cuadrada
+const abs = Math.abs(-200); //Convierte un número negativo en positivo
+const min = Math.min(3, 5, 1, 8, 2, 10); //El número mínimo de un listado
+const max = Math.max(3, 5, 1, 8, 2, 10); //el número máximo de un listado
+const random = Math.random(); //Número aleatorio
+const floorRandom = Math.floor(Math.random() * 30); //Combinación de funciones
+
+console.log('%cMath', 'color:yellow');
+console.log('pi: ', pi);
+console.log('round 2.5: ', round);
+console.log('ceil 2.1: ', ceil);
+console.log('floor 2.9: ', floor);
+console.log('sqrt 144: ', sqrt);
+console.log('abs -200: ', abs);
+console.log('min 3, 5, 1, 8, 2, 10: ', min);
+console.log('max 3, 5, 1, 8, 2, 10: ', max);
+console.log('random: ', random);
+console.log('Math.floor(Math.random() * 30) = ', floorRandom);
+
+//El orden de las operaciones es igual que en matemáticas
+
+//Incrementos
+console.log('%cIncrementos', 'color:yellow');
+
+let puntaje = 10;
+
+// Incremento en 1++
+
+puntaje++; //10
+++puntaje; //12
+
+puntaje--; //11
+
+console.log(--puntaje); //10
+
+puntaje += 10;
+
+console.log(puntaje);
+
+// Template String - String Literals
+
+console.log('%cTEMPLATE STRING', 'color:#11e6e8');
+
+const nombre = 'Juan';
+const email = 'correo@correo.com';
+
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
+
+//Booleans
+
+console.log('%cBOOLEANS', 'color:#11e6e8');
+
+const boolean1 = true;
+const boolean2 = false;
+
+console.log(boolean1);
+console.log(typeof boolean1);
+console.log(boolean2);
+console.log(typeof boolean2);
