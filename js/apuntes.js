5291319f0fdd6eade7e9785f5b68c07e3bd81003
Veronica Gonzalez <veronicagonzalez@serviciodigital.cl>
2023-02-11 00:45:48 -0300

    Add nuevos apuntes sobre objetos, uso stricto y correccion en la organizacion de los apuntes

--- js/apuntes.js
+++ js/apuntes.js
@@ -1,3 +1,6 @@
+// Ejecucion estricta: "use strict";
+"use strict";
+
 // Variables
 
 // let y var son similares
@@ -47,6 +50,15 @@ console.log('Cuando el elemento no es encontrado lanza el siguiente dato: ', twe
 console.log('%cIncludes', 'color:yellow');
 console.log('¿Se encuentra el elemento \"JavaScript\" en la cadena de texto \"tweet\"?', tweet.includes('JavaScript'));
 console.log('¿Se encuentra el elemento \"tablet\" en la cadena de texto \"tweet\"?', tweet.includes('tablet'));
+
+// Template String - String Literals
+
+console.log('%cTEMPLATE STRING', 'color:#11e6e8');
+
+const nombre = 'Juan';
+const email = 'correo@correo.com';
+
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
 
 // NUMEROS
 console.log('%cNUMEROS', 'color:#11e6e8');
@@ -110,16 +122,7 @@ puntaje += 10;
 
 console.log(puntaje);
 
-// Template String - String Literals
-
-console.log('%cTEMPLATE STRING', 'color:#11e6e8');
-
-const nombre = 'Juan';
-const email = 'correo@correo.com';
-
-console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
-
-//Booleans
+// BOOLEANS
 
 console.log('%cBOOLEANS', 'color:#11e6e8');
 
@@ -130,3 +133,119 @@ console.log(boolean1);
 console.log(typeof boolean1);
 console.log(boolean2);
 console.log(typeof boolean2);
+
+// OBJETOS
+
+console.log('%cOBJETOS', 'color:#11e6e8');
+
+const productoObj = {
+    nombreObj: "Monitor",
+    precio: 300,
+    disponible: true
+}
+
+console.log(productoObj);
+console.log('Nombre Producto: ', productoObj.nombreObj);
+console.log('Precio Monitor: ', productoObj.precio)
+console.log('Disponible: ', productoObj.disponible)
+
+// Otra sintaxis
+console.log('%cOtra sintaxis', 'color:yellow');
+console.log('Precio: ', productoObj["precio"])
+
+// Anadiendo nueva propiedad
+
+console.log('%cNueva Propiedad', 'color:yellow');
+productoObj.imagen = 'imagen.jpg';
+console.log(productoObj);
+
+// Eliminar Propiedades
+
+console.log('%cEliminar Propiedad Imagen', 'color:yellow');
+delete productoObj.imagen;
+console.log(productoObj);
+
+// Forma anterior
+// Crear variables desde propiedad
+
+console.log('%cVariables desde propiedad - Antiguo', 'color:yellow');
+const precioProducto = productoObj.precio;
+const nombreProducto = productoObj.nombreObj;
+
+console.log('Precio: ', precioProducto);
+console.log('Nombre producto: ', nombreProducto);
+
+// Destructuring
+
+console.log('%cDestructuring', 'color:yellow');
+
+const { precio, nombreObj } = productoObj;
+
+console.log('Precio: ', precio);
+console.log('Nombre producto: ', nombreObj)
+
+// Congela objeto
+
+console.log('%cObject Freeze', 'color:yellow');
+
+Object.freeze(productoObj)
+
+console.log('El objeto productoObj está congelado: ', Object.isFrozen(productoObj))
+
+/* Al intentar añadir una nueva propiedad al objeto, lanza un error. 
+Probar descomentando la siguiente linea */
+
+// productoObj.imagen = 'imagen.jpg';
+
+// Seal 
+
+console.log('%cObject Seal', 'color:yellow');
+
+const productoSeal = {
+    nombreSeal: "Monitor",
+    precio: 300,
+    disponible: true
+}
+
+Object.seal(productoSeal);
+
+/* Al intentar añadir una nueva propiedad al objeto, lanza un error. 
+Probar descomentando la siguiente linea */
+
+// productoObj.imagen = 'imagen.jpg';
+
+console.log('El objeto productoSeal is Seal: ', Object.isSealed(productoSeal));
+
+console.log('Producto sin modificar: ', productoSeal);
+
+/* Al modificar una propiedad ya existente, no hay ningún problema */
+
+productoSeal.precio = 'Nuevo Precio';
+
+console.log('Producto modificado: ', productoSeal);
+
+// Freeze no permite modificar nada
+// Seal permite modificar los existentes
+
+
+// Unir dos objetos 
+console.log('%cUnir objetos', 'color:yellow');
+
+const productoUnir = {
+    nombreUnir: "Monitor",
+    precio: 300,
+    disponible: true
+}
+
+console.log('Objeto uno: ', productoUnir);
+
+const medidasProducto = {
+    peso: '1kg',
+    pulgadas: '20 pulgadas',
+}
+
+console.log('Objeto dos: ', medidasProducto);
+
+const productoUnido = {...productoUnir, ...medidasProducto };
+
+console.log('Objetos unidos: ', productoUnido);
